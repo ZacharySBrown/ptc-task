b0736c783438708724786efae13c0a69c998d48e
zakbrown <zachary.s.brown@gmail.com>
2020-11-20 20:48:56 -0500

    cleaned up dataset

--- src/moner/data.py
+++ src/moner/data.py
@@ -51,39 +51,11 @@ class NERMultiOutput(Dataset):
         self._encode_label_indexes()
         self.all_outside = self._get_all_outside()
 
-        #self.tokens, self.token_starts, self.token_lengths = zip(*[self._get_token_starts(t) for t in self.tokens])
-        #_, self.token_starts, self.token_lengths = zip(*[self._get_token_starts(t) for t in self.tokens])
-
-        #self._get_label_indexes()
-        #self._encode_label_indexes()
-        #self.all_outside = self._get_all_outside()
-
-        #self.labels_aligned = [self._align_tokens_labels(labels, lengths) for labels, lengths in zip(self.labels_encoded, self.token_lengths)]
-        #self.labels_start_end = [[self.all_outside] + l + [self.all_outside] for l in self.labels_aligned]
-
-        #self.token_input = [self.tokenizer([tok], is_split_into_words=True, padding='max_length', max_length=self.max_length) for tok in self.tokens]
-
-        #self.subword_attention_mask = [[self.start_end_attn_val] + i + [self.start_end_attn_val] for i in self.token_starts]
-
-        #for i, mask, labels in zip(self.token_input, self.subword_attention_mask, self.labels_start_end):
-        #    i['attention_mask'] = mask
-        #    i['label'] = labels
-
-        #self.token_input = [
-        #    {
-        #        k: torch.LongTensor(v).to(self.device).unsqueeze(0)
-        #        for k, v in i.items()
-        #    } 
-        #    for i in self.token_input
-        #]
-
-
     def _parse_conll(self):
         """
         Parse the raw text of a CONLL2003 formatted seq dataset
         """
         raw = open(self.filepath).read().split('\n\n')
-        #raw = [i.strip('"') for i in raw]
         lines = [[i.split(self.sep) for i in r.split('\n') if i] for r in raw]
         lines = [[i for i in j if len(i[1:]) == self.num_labels] for j in lines]
         lines = [[ll for ll in l if len(ll[0]) > 0] for l in lines]
@@ -135,20 +107,10 @@ class NERMultiOutput(Dataset):
     def _get_token_starts(self, tokens):
 
         subword_tokenized = [self.tokenizer.tokenize(w) for w in tokens]
-        #subword_tokenized = [sw for sw in subword_tokenized if sw]
         flattened_subword_tokenized = list(chain.from_iterable(subword_tokenized))
         token_starts = list(chain.from_iterable([[1] + [0]*(len(sw)-1) for sw in subword_tokenized]))
         token_lengths = [len(sw) for sw in subword_tokenized]
         return flattened_subword_tokenized, subword_tokenized, token_starts, token_lengths
-
-        #subword_tokenized = list(map(self.tokenizer.tokenize, tokens))
-        #token_lengths = list(map(len, subword_tokenized))
-        #flattened_subword_tokenized = list(flatten(subword_tokenized))
-        #token_start_idxs = 1 + np.cumsum([0] + token_lengths[:-1])
-        #token_starts = [1 if it in token_start_idxs else 0 for it, _ in range(len(flattened_subword_tokenized))]
-        #return flattened_subword_tokenized, subword_tokenized, token_starts, token_lengths
-
-
 
     def _align_tokens_labels(self, labels_encoded, token_lengths):
         return list(chain.from_iterable([[lab] * l for lab, l in zip(labels_encoded, token_lengths)]))
@@ -159,10 +121,6 @@ class NERMultiOutput(Dataset):
     
     def __getitem__(self, idx):
 
-        #return {
-        #    k: torch.LongTensor(v).unsqueeze(0)
-        #    for k, v in self.token_input[idx].items()
-        #}
         tokens = self.tokens[idx]
         tokens_, subword_tokenized, token_starts, token_lengths = self._get_token_starts(tokens)
         
@@ -172,7 +130,6 @@ class NERMultiOutput(Dataset):
         labels_start_end = [self.all_outside] + labels_aligned[:self.max_length-2] + [self.all_outside]
         labels_start_end_padded = labels_start_end + (self.max_length - len(labels_start_end)) * [self.all_outside] 
 
-        #subword_attention_mask = [self.start_end_attn_val] + token_starts + [self.start_end_attn_val]
         subword_attention_mask = [self.start_end_attn_val] + token_starts[:self.max_length-2] + [self.start_end_attn_val]
         subword_attention_mask_padded = subword_attention_mask + (self.max_length - len(labels_start_end)) * [0] 
     
@@ -191,16 +148,5 @@ class NERMultiOutput(Dataset):
             for k, v in input_encoded.items()
         }
 
-        return {
-            'tokens_': tokens_, 
-            'tokens': self.tokens[idx],
-            'token_starts': token_starts, 
-            'token_lengths': token_lengths,
-            'subword_tokenized': subword_tokenized, 
-            'labels': self.labels[idx], 
-            'labels_aligned': labels_aligned, 
-            'labels_start_end': labels_start_end
-        }, a
-        #return self.token_input[idx]
         
 
